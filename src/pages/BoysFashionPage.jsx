5bbe2b5481da4fc46adc93007d74197cf04971cd
sugandv21 <sdvsuganya@gmail.com>
2025-08-07 23:09:45 +0530

    Update BoysFashionPage.jsx

--- src/pages/BoysFashionPage.jsx
+++ src/pages/BoysFashionPage.jsx
@@ -6,7 +6,9 @@ import BoysFashion from "../components/BoysFashion";
 
 const BoysFashionPage = () => {
   const [selectedFilter, setSelectedFilter] = useState(null);
-
+   useEffect(() => {
+    document.title = 'EarthBubs | Products | BoysFashion';
+  }, []);
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-6 max-w-7xl mx-auto">
       <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
@@ -49,4 +51,5 @@ const BoysFashionPage = () => {
 };
 
 export default BoysFashionPage;
+
 
