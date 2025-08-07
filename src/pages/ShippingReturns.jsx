1419d0ee0d8b563ce6dd406b379346c9782eacdc
sugandv21 <sdvsuganya@gmail.com>
2025-08-07 23:21:17 +0530

    Update ShippingReturns.jsx

--- src/pages/ShippingReturns.jsx
+++ src/pages/ShippingReturns.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   FaTruck,
   FaUndo,
@@ -96,4 +96,5 @@ const ShippingReturns = () => {
 };
 
 export default ShippingReturns;
+
 
