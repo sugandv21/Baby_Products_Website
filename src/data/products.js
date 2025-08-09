9c1f6966f019ff89fced88f1b772d5ffde54bd78
sugandv21 <sdvsuganya@gmail.com>
2025-08-09 08:43:43 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,96 @@
 
+import pampersImg1 from "../assets/images/Frame 1171274997.png";
+import pampersImg2 from "../assets/images/Frame 1171274998.png";
+import pampersImg3 from "../assets/images/Frame 1171274999.png";
+import pampersImg4 from "../assets/images/Frame 1171275002.png";
+import pampersImg5 from "../assets/images/Frame 1171275001.png";
+import pampersImg6 from "../assets/images/Frame 1171275000.png";
+
+import boysfasImg1 from "../assets/images/Frame 1171275003.png";
+import boysfasImg2 from "../assets/images/Frame 1171275004.png";
+import boysfasImg3 from "../assets/images/Frame 1171275005.png";
+import boysfasImg4 from "../assets/images/Frame 1171275006.png";
+import boysfasImg5 from "../assets/images/Frame 1171275007.png";
+import boysfasImg6 from "../assets/images/Frame 1171275008.png";
+
+import girlsfasImg1 from "../assets/images/Frame 1171275009.png";
+import girlsfasImg2 from "../assets/images/Frame 1171275010.png";
+import girlsfasImg3 from "../assets/images/Frame 1171275011.png";
+import girlsfasImg4 from "../assets/images/Frame 1171275012.png";
+import girlsfasImg5 from "../assets/images/Frame 1171275013.png";
+import girlsfasImg6 from "../assets/images/Frame 1171275014.png";
+
+import soapImg1 from "../assets/images/Frame 1171275015.png";
+import soapImg2 from "../assets/images/Frame 1171275016.png";
+import soapImg3 from "../assets/images/Frame 1171275017.png";
+import soapImg4 from "../assets/images/Frame 1171275019.png";
+import soapImg5 from "../assets/images/Frame 1171275015.png"; 
+import soapImg6 from "../assets/images/Frame 1171275018.png";
+
+import strollerImg1 from "../assets/images/Frame 1171275020.png";
+import strollerImg2 from "../assets/images/Frame 1171275021.png";
+import strollerImg3 from "../assets/images/Frame 1171275022.png";
+import strollerImg4 from "../assets/images/Frame 1171275023.png";
+import strollerImg5 from "../assets/images/Frame 1171275024.png";
+import strollerImg6 from "../assets/images/Frame 1171275025.png";
+
+import bottleImg1 from "../assets/images/bottle1.png";
+import bottleImg2 from "../assets/images/bottle2.png";
+import bottleImg3 from "../assets/images/bottle3.png";
+import bottleImg4 from "../assets/images/bottle4.png";
+import bottleImg5 from "../assets/images/bottle5.png";
+import bottleImg6 from "../assets/images/bottle6.png";
+
+export const pampersData = [
+  { id: 101, title: "Diaper Pants", images: [pampersImg1, pampersImg3, pampersImg2, pampersImg3], price: 680, mrp: 800, rating: 5 },
+  { id: 102, title: "Super Cute's", images: [pampersImg2, pampersImg3, pampersImg2, pampersImg3], price: 450, mrp: 500, rating: 4 },
+  { id: 103, title: "Mee Premium", images: [pampersImg3, pampersImg1, pampersImg2, pampersImg3], price: 700, mrp: 1044, rating: 5 },
+  { id: 104, title: "Mee Diaper Pants", images: [pampersImg4, pampersImg3, pampersImg2, pampersImg3], price: 580, mrp: 860, rating: 5 },
+  { id: 105, title: "Mee Pampers", images: [pampersImg5, pampersImg3, pampersImg2, pampersImg3], price: 800, mrp: 950, rating: 5 },
+  { id: 106, title: "Diaaper Combo", images: [pampersImg6, pampersImg3, pampersImg2, pampersImg3], price: 1300, mrp: 1850, rating: 5 },
+];
+
+export const boysfasData = [
+  { id: 201, title: "Mama Miei Baby", images: [boysfasImg1, boysfasImg3, boysfasImg2, boysfasImg3], price: 3199, mrp: 3600, rating: 5 },
+  { id: 202, title: "Caleb", images: [boysfasImg2, boysfasImg3, boysfasImg2, boysfasImg3], price: 1800, mrp: 2300, rating: 4 },
+  { id: 203, title: "Mee Mee Premium", images: [boysfasImg3, boysfasImg1, boysfasImg2, boysfasImg3], price: 1399, mrp: 2068, rating: 5 },
+  { id: 204, title: "Saphis", images: [boysfasImg4, boysfasImg3, boysfasImg2, boysfasImg3], price: 1800, mrp: 2100, rating: 5 },
+  { id: 205, title: "Archer", images: [boysfasImg5, boysfasImg3, boysfasImg2, boysfasImg3], price: 1399, mrp: 1456, rating: 5 },
+  { id: 206, title: "Marcus", images: [boysfasImg6, boysfasImg3, boysfasImg2, boysfasImg3], price: 1199, mrp: 1456, rating: 5 },
+];
+
+export const girlsfasData = [
+  { id: 301, title: "Halemons Floral", images: [girlsfasImg1, girlsfasImg3, girlsfasImg2, girlsfasImg3], price: 1399, mrp: 1466, rating: 5 },
+  { id: 302, title: "Halemons Victoria", images: [girlsfasImg2, girlsfasImg3, girlsfasImg2, girlsfasImg3], price: 1299, mrp: 1666, rating: 4 },
+  { id: 303, title: "Newborn Cutie", images: [girlsfasImg3, girlsfasImg1, girlsfasImg2, girlsfasImg3], price: 2399, mrp: 2488, rating: 5 },
+  { id: 304, title: "Foreto Baby", images: [girlsfasImg4, girlsfasImg3, girlsfasImg2, girlsfasImg3], price: 699, mrp: 856, rating: 5 },
+  { id: 305, title: "Zibuyu", images: [girlsfasImg5, girlsfasImg3, girlsfasImg2, girlsfasImg3], price: 499, mrp: 566, rating: 5 },
+  { id: 306, title: "Poppes", images: [girlsfasImg6, girlsfasImg3, girlsfasImg2, girlsfasImg3], price: 399, mrp: 466, rating: 5 },
+];
+
+export const soapData = [
+  { id: 401, title: "Gentle Baby Soap", images: [soapImg1, soapImg3, soapImg2, soapImg3], price: 380, mrp: 466, rating: 5 },
+  { id: 402, title: "Baby Soap", images: [soapImg2, soapImg3, soapImg2, soapImg3], price: 325, mrp: 500, rating: 4 },
+  { id: 403, title: "Natural Soap", images: [soapImg3, soapImg1, soapImg2, soapImg3], price: 599, mrp: 600, rating: 5 },
+  { id: 404, title: "Pro Baby Soap", images: [soapImg4, soapImg3, soapImg2, soapImg3], price: 320, mrp: 470, rating: 5 },
+  { id: 405, title: "Mee Baby Soap", images: [soapImg5, soapImg3, soapImg2, soapImg3], price: 399, mrp: 466, rating: 5 },
+  { id: 406, title: "Milk Baby Soap", images: [soapImg6, soapImg3, soapImg2, soapImg3], price: 800, mrp: 980, rating: 5 },
+];
+
+export const strollerData = [
+  { id: 501, title: "Twin Baby", images: [strollerImg1, strollerImg3, strollerImg2, strollerImg3], price: 4399, mrp: 5466, rating: 5 },
+  { id: 502, title: "Baby Rocker", images: [strollerImg2, strollerImg3, strollerImg2, strollerImg3], price: 8399, mrp: 9466, rating: 4 },
+  { id: 503, title: "Lavern", images: [strollerImg3, strollerImg1, strollerImg2, strollerImg3], price: 9399, mrp: 10466, rating: 5 },
+  { id: 504, title: "Rock Roll", images: [strollerImg4, strollerImg3, strollerImg2, strollerImg3], price: 3399, mrp: 4800, rating: 5 },
+  { id: 505, title: "Kinger", images: [strollerImg5, strollerImg3, strollerImg2, strollerImg3], price: 6000, mrp: 7000, rating: 5 },
+  { id: 506, title: "Dravo", images: [strollerImg6, strollerImg3, strollerImg2, strollerImg3], price: 4399, mrp: 5466, rating: 5 },
+];
+
+export const bottleData = [
+  { id: 601, title: "Mee Steele", images: [bottleImg1, bottleImg3, bottleImg2, bottleImg3], price: 399, mrp: 466, rating: 5 },
+  { id: 602, title: "LuvLap", images: [bottleImg2, bottleImg3, bottleImg2, bottleImg3], price: 240, mrp: 300, rating: 4 },
+  { id: 603, title: "Philips", images: [bottleImg3, bottleImg1, bottleImg2, bottleImg3], price: 599, mrp: 600, rating: 5 },
+  { id: 604, title: "Aveta", images: [bottleImg4, bottleImg3, bottleImg2, bottleImg3], price: 250, mrp: 300, rating: 5 },
+  { id: 605, title: "Chicco", images: [bottleImg5, bottleImg3, bottleImg2, bottleImg3], price: 400, mrp: 499, rating: 5 },
+  { id: 606, title: "Small Wonder", images: [bottleImg6, bottleImg3, bottleImg2, bottleImg3], price: 399, mrp: 480, rating: 5 },
+];
